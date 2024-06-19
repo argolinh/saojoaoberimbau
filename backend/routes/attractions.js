5ecfe0c6775642f69d17e178f6ff22fe072b4e1d
argolinh <sosconnect39@gmail.com>
2024-06-19 19:43:25 -0300

    Delete backend directory

--- backend/routes/attractions.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const attractionsController = require('../controllers/attractions');
-
-router.get('/', attractionsController.getAttractions);
-router.post('/', attractionsController.addAttraction);
-
-module.exports = router;
